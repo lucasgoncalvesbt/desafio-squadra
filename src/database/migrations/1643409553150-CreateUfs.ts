4bc34f26045d689d077d2638950cf3f306495dc2
Lucas Gonçalves <lucasgoncalves.jobs@gmail.com>
2022-01-28 19:55:53 -0300

    feat:  adiciona migration de uf

---
+++ src/database/migrations/1643409553150-CreateUfs.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUfs1643409553150 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tb_uf',
+        columns: [
+          {
+            name: 'codigo_uf',
+            type: 'integer',
+            isPrimary: true,
+          },
+          {
+            name: 'sigla',
+            type: 'varchar',
+          },
+          {
+            name: 'nome',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'integer',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tb_uf');
+  }
+}
